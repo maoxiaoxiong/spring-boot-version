9617f80b0ffb340e43ded56a16188b40c5258840
maoxiaoxiong <1064517852@qq.com>
2019-11-12 13:51:14 +0800

    整合jpa_hibernate

--- springdatas/jap_hibernate/src/main/java/com/jpa/hibernate/jap/hibernate/config/DruidConfiguration.java
+++ springdatas/jap_hibernate/src/main/java/com/jpa/hibernate/jap/hibernate/config/DruidConfiguration.java
@@ -1,14 +1,6 @@
 package com.jpa.hibernate.jap.hibernate.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-
-import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
